5b087b9f94e1c502c2d8ed12a61f351b355d537b
Ryan Shaw <ryan@instructure.com>
2015-05-27 00:39:55 -0600

    add shrinkwrap

--- src/cli.js
+++ src/cli.js
@@ -3,15 +3,15 @@ import main from './main'
 import compileSingleBundle from './compile-bundle'
 
 program
+  .version(require('../package').version)
   .option('--watch', 'watch for changes')
-  .option('--optional')
   .option('--bundle <bundle>', 'compile a specific bundle')
   .option('--variant <variant>', 'the variant (legacy_high_contrast, new_styles_normal_contrast, etc) you want')
   .option('--brand-variables-folder <brandVarablesFolder>', 'filesystem path to the folder that contains the _brand_variables.scss you want to use')
 
 program.parse(process.argv)
 
-// if they secified the specific bundle they want, (eg: from the brand_css rails controller),
+// if they specified the specific bundle they want, (eg: from the brand_css rails controller),
 // compile that one bundle and send it to stdout
 if (program.bundle) {
   compileSingleBundle({
