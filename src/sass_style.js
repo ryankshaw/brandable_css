72f179d91e7ebe2faebc5d6c1ca8a8dd8d35bb5c
Ryan Shaw <ryan@instructure.com>
2015-06-06 17:34:29 -0600

    fixup

--- src/sass_style.js
+++ src/sass_style.js
@@ -1,6 +1,7 @@
 // If you want compressed output (eg: in production), set the environment
 // variable SASS_STYLE=compressed. For rails apps, we'll fall back to a sane
 // default of using "compressed" if RAILS_ENV == production
-export default const SASS_STYLE = process.env.SASS_STYLE || 
-                                  (process.env.RAILS_ENV == 'production' ? 'compressed' : 'nested')
+const SASS_STYLE = process.env.SASS_STYLE ||
+                   (process.env.RAILS_ENV == 'production' ? 'compressed' : 'nested')
 
+export default  SASS_STYLE
