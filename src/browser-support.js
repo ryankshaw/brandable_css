d10a161ecef930923576d8a13c5b91a2e1d4a796
Ryan Shaw <ryan@instructure.com>
2015-07-06 14:14:41 -0600

    fix reference to non-existant paths file

--- src/browser-support.js
+++ src/browser-support.js
@@ -1,8 +1,8 @@
 import _ from 'lodash'
 import yaml from 'js-yaml'
 import fs from 'fs'
-import {browsers_yml} from './paths'
+import paths from './config'
 
-export default _.map(yaml.safeLoad(fs.readFileSync(browsers_yml)).minimums, (version, browserName) => {
+export default _.map(yaml.safeLoad(fs.readFileSync(paths.browsers_yml)).minimums, (version, browserName) => {
   return browserName.replace('Internet Explorer', 'Explorer') + ' >= ' + version
 })
