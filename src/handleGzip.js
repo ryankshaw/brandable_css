121b08f8d77feadba343c628ec3a4ee242d68dd9
Ryan Shaw <ryan@instructure.com>
2016-06-02 14:04:20 -0600

    fixup

--- src/handleGzip.js
+++ src/handleGzip.js
@@ -7,7 +7,7 @@ import {debug} from './utils'
 export default async function handleGzip (params) {
   const css = params.Body
   if (css.length > 150) { // gzipping small files is not worth it
-    const gzipped = await gzip(css, { level : zlib.Z_BEST_COMPRESSION })
+    const gzipped = await gzip(css, { level: zlib.Z_BEST_COMPRESSION })
     const compression = Math.round(100 - (100.0 * gzipped.length / css.length))
 
     // If we couldn't compress more than 5%, the gzip decoding cost to the
