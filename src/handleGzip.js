d4c3824c30029e9b73b6d6de01b450cf595235d8
Ryan Shaw <ryan@instructure.com>
2016-06-02 13:55:06 -0600

    fix gzip

--- src/handleGzip.js
+++ src/handleGzip.js
@@ -1,7 +1,5 @@
 import {promisify} from 'bluebird'
-import zlib from 'zlib'
-// compress stuff as much as possible, even if it is slower. since it is a one-time cost here
-zlib.Z_DEFAULT_COMPRESSION = zlib.Z_BEST_COMPRESSION
+import {zlib} from 'zlib'
 const gzip = promisify(zlib.gzip)
 
 import {debug} from './utils'
@@ -9,7 +7,7 @@ import {debug} from './utils'
 export default async function handleGzip (params) {
   const css = params.Body
   if (css.length > 150) { // gzipping small files is not worth it
-    const gzipped = await gzip(new Buffer(css))
+    const gzipped = await gzip(css, { level : zlib.Z_BEST_COMPRESSION })
     const compression = Math.round(100 - (100.0 * gzipped.length / css.length))
 
     // If we couldn't compress more than 5%, the gzip decoding cost to the
